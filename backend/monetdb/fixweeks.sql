9534b9384f6c53208bdab270dd0dec3ff27b0600
Hannes Mühleisen <hannes@muehleisen.org>
2013-07-05 08:28:38 +0200

    week fix

---
+++ backend/monetdb/fixweeks.sql
@@ -0,0 +1,26 @@
+-- fix shorter weeks
+-- first and last week in year may be shorter
+--First Week Days
+--2008: 6
+UPDATE weekcountenf SET "count"="count"*1.14 where "year"=2008 and "week"=1;
+--2009: 4
+UPDATE weekcountenf SET "count"="count"*1.42 where "year"=2009 and "week"=1;
+--2010: 6
+UPDATE weekcountenf SET "count"="count"*1.14 where "year"=2010 and "week"=1;
+--2011: 7
+--2012: 7
+--2013: 6
+UPDATE weekcountenf SET "count"="count"*1.14 where "year"=2013 and "week"=1;
+
+--Last Week Days
+--2008: 7 (52)
+--2009: 4 (53)
+UPDATE weekcountenf SET "count"="count"*1.42 where "year"=2009 and "week"=53;
+--2010: 5 (52)
+UPDATE weekcountenf SET "count"="count"*1.28 where "year"=2010 and "week"=52;
+--2011: 6 (52)
+UPDATE weekcountenf SET "count"="count"*1.14 where "year"=2010 and "week"=52;
+--2012: 7 (52)
+--2013: 7 (52)
+
+
